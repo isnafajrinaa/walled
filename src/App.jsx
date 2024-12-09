311f8911b0a7fa23a2e000456c36f1bd79ba3d8c
isnafajrinaa <fajrinaisna@student.ub.ac.id>
2024-12-09 13:51:22 +0700

    update onClick state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import './App.css'
+import { useState } from "react";
+import logo from "./assets/logo.png";
+import NavItems from "./components/NavItems";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("Dashboard");
+  const menu = [
+    {
+      title: "Dashboard",
+      link: "#",
+    },
+    {
+      title: "Transfer",
+      link: "#",
+    },
+    {
+      title: "Topup",
+      link: "#",
+    },
+    {
+      title: "Signout",
+      link: "#",
+    },
+  ];
+
   return (
     <>
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <nav className="flex items-center w-screen justify-between bg-white py-4 px-8">
+        <img src={logo} />
+        <div className="flex gap-x-8">
+          <NavItems
+            menu={menu}
+            activeTab={activeTab}
+            handleClick={setActiveTab}
+          />
+        </div>
+      </nav>
     </>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/NavItems.jsx
@@ -0,0 +1,17 @@
+//Destructuring = memanggil semua objek sekaligus
+// destructuring js -> {menu}
+// https://www.w3schools.com/js/js_destructuring.asp
+
+function NavItems({ menu, activeTab, handleClick }) {
+  return (
+    <div className="flex gap-x-8">
+      {menu.map((item, index) => (
+        <a onClick={() => handleClick(item.title)} className={activeTab !== item.title ? 'text-black' : 'text[19918F] font-bold'} key={index} href={item.link}>
+          {item.title}
+        </a>
+      ))}
+    </div>
+  );
+}
+
+export default NavItems;
